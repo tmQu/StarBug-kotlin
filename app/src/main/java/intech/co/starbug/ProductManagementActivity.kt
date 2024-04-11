0d4e3d9922cccd981e1fb503ebd2e314d897c5ea
HuyBean <nqhuy0711@gmail.com>
2024-04-11 15:40:13 +0700

    Huy - Finish Product Management Page

--- app/src/main/java/intech/co/starbug/ProductManagementActivity.kt
+++ app/src/main/java/intech/co/starbug/ProductManagementActivity.kt
@@ -1,20 +1,107 @@
 package intech.co.starbug
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import intech.co.starbug.model.ProductModel
 
 class ProductManagementActivity : AppCompatActivity() {
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var productsRef: DatabaseReference
+    private lateinit var adapter: ProductAdapter
+    private lateinit var recyclerViewProducts: RecyclerView
+
+    private lateinit var buttonAddProduct: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_product_management)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Khởi tạo recyclerViewProducts
+        recyclerViewProducts = findViewById(R.id.recyclerViewProducts)
+
+        database = FirebaseDatabase.getInstance()
+        productsRef = database.getReference("Products")
+
+        adapter = ProductAdapter()
+        recyclerViewProducts.layoutManager = LinearLayoutManager(this)
+        recyclerViewProducts.adapter = adapter
+
+        productsRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val products = mutableListOf<ProductModel>()
+                for (snapshot in dataSnapshot.children) {
+                    val product = snapshot.getValue(ProductModel::class.java)
+                    product?.let { products.add(it) }
+                }
+                adapter.submitList(products)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("ProductManagement", "Failed to read value.", databaseError.toException())
+            }
+        })
+
+        adapter.setOnDetailClickListener { product ->
+            // Chuyển sang UpdateProductManagementActivity và truyền ID sản phẩm
+            val intent = Intent(this@ProductManagementActivity, UpdateProductManagementActivity::class.java)
+            intent.putExtra("PRODUCT_ID", product.id)
+            startActivity(intent)
         }
+
+
+        // Trong phương thức onCreate của ProductManagementActivity
+        adapter.setOnDeleteClickListener { product ->
+            val alertDialogBuilder = AlertDialog.Builder(this@ProductManagementActivity)
+            alertDialogBuilder.apply {
+                setTitle("Confirm Delete")
+                setMessage("Are you sure you want to delete this product?")
+                setPositiveButton("Yes") { _, _ ->
+                    // Xóa sản phẩm từ cơ sở dữ liệu Firebase
+                    productsRef.child(product.id).removeValue()
+                        .addOnSuccessListener {
+                            // Xử lý thành công
+                            Toast.makeText(
+                                this@ProductManagementActivity,
+                                "Product deleted successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        .addOnFailureListener { e ->
+                            // Xử lý thất bại
+                            Log.e("ProductManagement", "Error deleting product", e)
+                            Toast.makeText(
+                                this@ProductManagementActivity,
+                                "Failed to delete product",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                }
+                setNegativeButton("No") { dialog, _ ->
+                    dialog.dismiss()
+                }
+            }
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
+
+        buttonAddProduct = findViewById(R.id.buttonAddProduct)
+        buttonAddProduct.setOnClickListener {
+            val intent = Intent(this@ProductManagementActivity, AddProductManagementActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/intech/co/starbug/adapter/ProductAdapter.kt
@@ -0,0 +1,98 @@
+package intech.co.starbug
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import intech.co.starbug.model.ProductModel
+
+class ProductAdapter(
+    private var itemList: List<ProductModel> = emptyList(),
+    private var onDetailClick: ((ProductModel) -> Unit)? = null,
+    private var onDeleteClick: ((ProductModel) -> Unit)? = null
+) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+    var onItemClick: ((View, Int) -> Unit)? = null
+    private lateinit var context: Context // Thêm một biến context
+
+    inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemImageView: ImageView = itemView.findViewById(R.id.imageViewProduct)
+        val itemNameTextView: TextView = itemView.findViewById(R.id.textViewProductName)
+        val itemPriceTextView: TextView = itemView.findViewById(R.id.textViewNumPrice)
+        val itemCategoryTextView: TextView = itemView.findViewById(R.id.textViewTxtCategory)
+        val detailButton: Button = itemView.findViewById(R.id.detailButton)
+        val deleteImageButton: ImageButton = itemView.findViewById(R.id.deleteImageButton)
+        init {
+            itemView.setOnClickListener {
+                onItemClick?.invoke(it, adapterPosition)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        context = parent.context // Lấy context từ ViewGroup
+        val itemView = LayoutInflater.from(context).inflate(R.layout.product_management_item, parent, false)
+        val viewHolder = ProductViewHolder(itemView)
+
+        // Xử lý sự kiện click cho nút "Detail"
+        viewHolder.detailButton.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                val product = itemList[position]
+                onDetailClick?.invoke(product)
+            }
+        }
+
+        // Xử lý sự kiện click cho nút "Delete"
+        viewHolder.deleteImageButton.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                val product = itemList[position]
+                onDeleteClick?.invoke(product)
+            }
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val currentItem = itemList[position]
+        holder.itemNameTextView.text = currentItem.name
+        holder.itemPriceTextView.text = currentItem.price.toString()
+        holder.itemCategoryTextView.text = currentItem.category
+        Picasso.get().load(currentItem.img[0]).into(holder.itemImageView)
+
+    }
+
+    override fun getItemCount() = itemList.size
+
+    fun submitList(list: List<ProductModel>) {
+        itemList = list
+        notifyDataSetChanged()
+    }
+
+    // Phương thức public để thiết lập listener cho sự kiện nhấp vào item
+    fun setOnItemClickListener(listener: (ProductModel) -> Unit) {
+        onItemClick = { view, position ->
+            val item = itemList[position]
+            listener.invoke(item)
+        }
+    }
+
+    fun setOnDetailClickListener(listener: (ProductModel) -> Unit) {
+        onDetailClick = listener
+    }
+
+    fun setOnDeleteClickListener(listener: (ProductModel) -> Unit) {
+        onDeleteClick = listener
+    }
+
+
+}
